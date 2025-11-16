5a3b63515ea77874dee214e32797218c9ee96017
carol <carolineallen1012@gmail.com>
2025-11-15 21:23:50 -0500

    functions lab completed

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 
+function calculateTax(amount) {
+    const taxRate = 0.10;
+    return amount * taxRate;
+}
+
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
+
+function findMaximum (num1, num2){
+    if (num1 > num2) {
+        return num1
+    } else {
+        return num2
+    }
+}
+
+function isPalindrome (word) {
+    const reversedWord = word.split("").reverse().join("")
+    return reversedWord === word
+}
+
+function calculateDiscountedPrice (originalPrice, discountPercentage){
+    const discount = (originalPrice * (discountPercentage/100))
+
+    const newPrice = originalPrice - discount
+
+    return newPrice
+
+}
+
+
 
 
 
